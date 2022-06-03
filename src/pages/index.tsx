229774255bd18d97b412e84b524cafac388b75c7
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-06-03 00:02:28 -0300

    criando os components de titulo e subtitulo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import type { NextPage } from 'next'
+import Titulo from '../ui/components/titulo/titulo';
 
 const Home: NextPage = () => {
   return (
     <div>
-
+      <Titulo 
+        titulo="" 
+        subtitulo={
+          <span>
+            Com um pegueno valor mensal, você <br/>
+            pode <strong>adotar um pet virtualmente</strong>
+          </span>
+        }/>
     </div>
   )
 }
