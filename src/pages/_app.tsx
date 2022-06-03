5d53bc55e68bdf493a41f633a5b390b81214e98c
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-06-02 22:28:52 -0300

    criando o cabecalho do projeto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../ui/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@mui/material';
+import tema from '../ui/themes/tema';
+import Cabecalho from '../ui/components/cabecalho/cabecalho'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={tema}>
+        <Cabecalho />
+        <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp
