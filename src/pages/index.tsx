fefa623664ecc943205a80320db73a6d550799f4
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-06-04 00:09:34 -0300

    criando os components de listagem de pets

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Titulo from '../ui/components/titulo/titulo';
+import Lista from '../ui/components/lista/lista';
 
 const Home: NextPage = () => {
   return (
@@ -12,6 +13,23 @@ const Home: NextPage = () => {
             pode <strong>adotar um pet virtualmente</strong>
           </span>
         }/>
+
+      <Lista 
+        pets={[
+          {
+            id: 1,
+            nome: 'Bidu',
+            historia: 'aoskdasodksaodkasokdoasodkoaskd',
+            foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFagTiTpNY1gk2vXKKj6ZKPbPm8rJuHthJRw&usqp=CAU'
+          },
+          {
+            id: 2,
+            nome: 'Scooby',
+            historia: 'É um animal muito dócil.',
+            foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjx95LKp11nXxOq-nFnUkuvVtUDxfnDnGdLQ&usqp=CAU'
+          },
+        ]}
+      />
     </div>
   )
 }
